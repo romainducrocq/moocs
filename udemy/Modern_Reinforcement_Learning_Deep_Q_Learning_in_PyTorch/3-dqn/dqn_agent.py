74260f1549bb0b2b5c5ed0403df14bf2968de2e7
romainducrocq <romain.g.ducrocq@gmail.com>
2021-04-18 19:59:31 +0200

    section 4, refactor

---
+++ udemy/Modern_Reinforcement_Learning_Deep_Q_Learning_in_PyTorch/3-dqn/dqn_agent.py
@@ -0,0 +1,95 @@
+import numpy as np
+import torch as T
+from deep_q_network import DeepQNetwork
+from replay_memory import ReplayBuffer
+
+
+class DQNAgent:
+    def __init__(self, gamma, epsilon, lr, n_actions, input_dims, mem_size, batch_size, eps_min=0.01, eps_dec=5e-7,
+                 update_target_freq=1000, algo=None, env_name=None, chkpt_dir='tmp/dqn'):
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.lr = lr
+        self.n_actions = n_actions
+        self.input_dims = input_dims
+        self.batch_size = batch_size
+        self.eps_min = eps_min
+        self.eps_dec = eps_dec
+        self.update_target_freq = update_target_freq
+        self.algo = algo
+        self.env_name = env_name
+        self.chkpt_dir = chkpt_dir
+        self.action_space = [i for i in range(self.n_actions)]
+        self.learn_step_counter = 0
+
+        self.memory = ReplayBuffer(mem_size, input_dims, n_actions)
+
+        self.q_online = DeepQNetwork(self.lr, self.n_actions, input_dims=self.input_dims,
+                                     name=self.env_name+'_'+self.algo+''+'_q_online', chkpt_dir=self.chkpt_dir)
+
+        self.q_target = DeepQNetwork(self.lr, self.n_actions, input_dims=self.input_dims,
+                                     name=self.env_name+'_'+self.algo+'_q_target', chkpt_dir=self.chkpt_dir)
+
+    def choose_action(self, observation):
+        if np.random.random() > self.epsilon:
+            state_t = T.tensor([observation], dtype=T.float).to(self.q_online.device)
+            actions_t = self.q_online.forward(state_t)
+            action = T.argmax(actions_t).item()
+        else:
+            action = np.random.choice(self.action_space)
+
+        return action
+
+    def store_transition(self, state, action, reward, new_state, done):
+        self.memory.store_transition(state, action, reward, new_state, done)
+
+    def sample_memory(self):
+        states, actions, rewards, new_states, dones = self.memory.sample_buffer(self.batch_size)
+
+        states_t = T.tensor(states).to(self.q_online.device)
+        actions_t = T.tensor(actions).to(self.q_online.device)
+        rewards_t = T.tensor(rewards).to(self.q_online.device)
+        new_states_t = T.tensor(new_states).to(self.q_online.device)
+        dones_t = T.tensor(dones).to(self.q_online.device)
+
+        return states_t, actions_t, rewards_t, new_states_t, dones_t
+
+    def update_target_network(self):
+        if self.learn_step_counter % self.update_target_freq == 0:
+            self.q_target.load_state_dict(self.q_online.state_dict())
+
+    def decrement_epsilon(self):
+        self.epsilon = self.epsilon - self.eps_dec if self.epsilon > self.eps_min else self.eps_min
+
+    def save_models(self):
+        self.q_online.save_checkpoint()
+        self.q_target.save_checkpoint()
+
+    def load_models(self):
+        self.q_online.load_checkpoint()
+        self.q_target.load_checkpoint()
+
+    def learn(self):
+        if self.memory.mem_cntr < self.batch_size:
+            return
+
+        self.q_online.optimizer.zero_grad()
+
+        self.update_target_network()
+
+        states_t, actions_t, rewards_t, new_states_t, dones_t = self.sample_memory()
+
+        indices = np.arange(self.batch_size)
+        q_pred = self.q_online.forward(states_t)[indices, actions_t]
+
+        q_target_max = self.q_target.forward(new_states_t).max(dim=1)[0]
+        q_target_max[dones_t] = 0.0
+
+        q_target = rewards_t + self.gamma*q_target_max
+
+        loss = self.q_online.loss(q_target, q_pred).to(self.q_online.device)
+        loss.backward()
+        self.q_online.optimizer.step()
+        self.learn_step_counter += 1
+
+        self.decrement_epsilon()
--- udemy/Modern_Reinforcement_Learning_Deep_Q_Learning_in_PyTorch/README.md
+++ udemy/Modern_Reinforcement_Learning_Deep_Q_Learning_in_PyTorch/README.md
@@ -5,12 +5,12 @@
 
 ### Syllabus
 
-**Section 1**
+**Section 1: Introduction**
 - 1. What You Will Learn In This Course  
 - 2. Required Background, software, and hardware  
 - 3. How to Succeed in this Course  
 
-**Section 2**
+**Section 2: Fundamentals of Reinforcement Learning**
 - 4. Agents, Environments, and Actions  
 - 5. Markov Decision Processes  
 - 6. Value Functions, Action Value Functions, and the Bellman Equation  
@@ -18,13 +18,29 @@
 - 8. The Explore-Exploit Dilemma  
 - 9. Temporal Difference Learning  
 
-**Section 3**
+**Section 3: Deep Learning Crash Course**
 - 10. Dealing with Continuous State Spaces with Deep Neural Networks  
 - 11. Naive Deep Q Learning in Code: Step 1 - Coding the Deep Q Network  
 - 13. Naive Deep Q Learning in Code: Step 3 - Coding the Main Loop and Learning  
 - 14. Naive Deep Q Learning in Code: Step 4 - Verifying the Functionality of Our Code  
 - 15. Naive Deep Q Learning in Code: Step 5 - Analyzing Our Agent's Performance  
 - 16. Dealing with Screen Images with Convolutional Neural Networks  
+
+**Section 4: Human Level Control Through Deep Reinforcement Learning, From Paper to Code**
+- 17. How to Read Deep Learning Papers
+- 18. Analyzing the Paper
+- 19. How to Modify the OpenAI Gym Atari Environments
+- 20. How to Preprocess the OpenAI Gym Atari Screen Images
+- 21. How to Stack the Preprocessed Atari Screen Images
+- 22. How to Combine All the Changes
+- 23. How to Add Reward Clipping, Fire First, and No Ops
+- 24. How to Code the Agent's Memory
+- 25. How to Code the Deep Q Network
+- 26. Coding the Deep Q Agent: Step 1 - Coding the Constructor
+- 27. Coding the Deep Q Agent: Step 2 - Epsilon-Greedy Action Selection
+- 28. Coding the Deep Q Agent: Step 3 - Memory, Model Saving and Network Copying
+- 29. Coding the Deep Q Agent: Step 4 - The Agent's Learn Function
+- 30. Coding the Deep Q Agent: Step 5 - The Main Loop and Analyzing the Performance
 
 ****
 
@@ -41,7 +57,7 @@
 > source venv/bin/activate  
 > deactivate  
 
-- torch, torchvision, numpy, matplotlib, gym, box2d-py, atari-py  
-> (venv) pip3 install torch torchvision numpy matplotlib gym box2d-py atari-py  
+- torch, torchvision, numpy, matplotlib, gym, box2d-py, atari-py, opencv-python  
+> (venv) pip3 install torch torchvision numpy matplotlib gym box2d-py atari-py opencv-python  
 
 
