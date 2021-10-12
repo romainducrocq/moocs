3f1b882e066f2cc3c0246ae300e8cabc73790591
romainducrocq <romain.g.ducrocq@gmail.com>
2021-10-13 00:38:22 +0200

    sfml2

--- udemy/SMFL2_Developing_2D_Games_with_C++/section1/2_sfml-works/README.md
+++
@@ -1,2 +0,0 @@
-sudo apt-get install libsfml-dev
-cd bin && ./build.sh && ./compile.sh && ./run.sh
--- udemy/SMFL2_Developing_2D_Games_with_C++/section5/11_audio/bin/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cmake -G "Unix Makefiles" -S ../build/ -B ../build/out/ -DCMAKE_BUILD_TYPE=Debug
